07b5063d58f124694cc69a6014db51f3a0057eda
Upasna Khatiwala <200573736@student.georgianc.on.ca>
2023-08-10 05:41:44 -0400

    added authentication (login and register) and stored the users data on Mongodb Atlas. Also implemented notification

--- app/src/main/java/com/mdev/tranquil/Login.kt
+++ app/src/main/java/com/mdev/tranquil/Login.kt
@@ -1,50 +1,96 @@
 package com.mdev.tranquil
 
-import DatabaseHelper
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
-
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
 
 class Login : AppCompatActivity() {
+
+    private val client = OkHttpClient()
+    private val BASE_URL = "http://10.0.2.2:3000" // Replace with your server's IP address
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        emailEditText = findViewById(R.id.editTextTextEmailAddress)
+        passwordEditText = findViewById(R.id.editTextTextPassword)
     }
 
-    fun goToSignUp(view: View) {
+    fun goToSignup(view: View) {
         val intent = Intent(this, Signup::class.java)
         startActivity(intent)
     }
 
-    fun startOTPActivity(view: View?) {
-
-        val emailEditText = findViewById<EditText>(R.id.editTextTextEmailAddress)
-        val passwordEditText = findViewById<EditText>(R.id.editTextTextPassword)
-
+    fun startOTPActivity(view: View) {
         val email = emailEditText.text.toString()
         val password = passwordEditText.text.toString()
 
-        // check user's credentials dynamically
-        val dbHandler = DatabaseHelper(this)
-        val user = dbHandler.getUser(email)
-
-        if (user != null && user.password == password) {
-            // credentials are correct, start OTP activity
-            val intent = Intent(this, OTP::class.java)
-            startActivity(intent)
-        } else {
-            // credentials are incorrect, show an alert dialog
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("Incorrect Credentials")
-            builder.setMessage("The email and password combination entered is incorrect.")
-            builder.setPositiveButton("OK", null)
-            val dialog = builder.create()
-            dialog.show()
+        val jsonObject = JSONObject()
+        try {
+            jsonObject.put("username", email)
+            jsonObject.put("password", password)
+        } catch (e: JSONException) {
+            e.printStackTrace()
         }
+
+        val JSON = "application/json; charset=utf-8".toMediaType()
+        val body = RequestBody.create(JSON, jsonObject.toString())
+
+        val request = Request.Builder()
+            .url("$BASE_URL/login") // Replace with your login endpoint
+            .post(body)
+            .build()
+
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val response = client.newCall(request).execute()
+                if (response.isSuccessful) {
+                    // Login successful, start OTP activity
+                    val intent = Intent(this@Login, OTP::class.java)
+                    startActivity(intent)
+                } else {
+                    // Login failed, show error dialog
+                    runOnUiThread {
+                        AlertDialog.Builder(this@Login)
+                            .setTitle("Login Error")
+                            .setMessage("Invalid email or password. Please check your credentials.")
+                            .setPositiveButton("OK", null)
+                            .create()
+                            .show()
+                    }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+                // Handle error, e.g., display an error dialog
+                runOnUiThread {
+                    AlertDialog.Builder(this@Login)
+                        .setTitle("Error")
+                        .setMessage("Failed to connect to the server. Please try again later.")
+                        .setPositiveButton("OK", null)
+                        .create()
+                        .show()
+                }
+            }
+        }
+    }
+
+    // Extension function for creating MediaType
+    private fun String.toMediaType(): MediaType {
+        return this.toMediaTypeOrNull()!!
     }
 }
--- app/src/main/java/com/mdev/tranquil/MainActivity.kt
+++ app/src/main/java/com/mdev/tranquil/MainActivity.kt
@@ -1,21 +1,104 @@
 package com.mdev.tranquil
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    // Notification channel ID
+    private val CHANNEL_ID = "my_channel_id"
+    // Replace this with your desired notification ID
+    private val notificationId = 1
+
+    // Internet permission request code
+    companion object {
+        private const val PERMISSION_REQUEST_INTERNET = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val startButton = findViewById<Button>(R.id.start_button)
 
         startButton.setOnClickListener {
             val intent = Intent(this, Signup::class.java)
             startActivity(intent)
+
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.INTERNET
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                createNotificationChannel()
+                showNotification("Daily Motivation", "Have faith in yourself and you will achieve all")
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.INTERNET),
+                    PERMISSION_REQUEST_INTERNET
+                )
+            }
+        }
+    }
+
+    // Create a notification channel (only needed for Android 8.0 and higher)
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "My Channel"
+            val descriptionText = "Channel Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = descriptionText
+
+            val notificationManager: NotificationManager? =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+            notificationManager?.createNotificationChannel(channel)
+        }
+    }
+
+    // Show a notification
+    private fun showNotification(textTitle: String, textContent: String) {
+        val builder = NotificationCompat.Builder(this@MainActivity, CHANNEL_ID)
+            .setSmallIcon(R.drawable.notification_icon)
+            .setContentTitle(textTitle)
+            .setContentText(textContent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+        notificationManager?.notify(notificationId, builder.build())
+    }
+
+    // Handle permission request result
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSION_REQUEST_INTERNET) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                createNotificationChannel()
+                showNotification("Notification Title", "Notification Content")
+            } else {
+                // Handle the case where permission was denied
+                Toast.makeText(this, "Internet permission denied.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
